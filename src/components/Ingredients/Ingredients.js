73439a690e2e6866eaaeb2fac0b8117c1807a9f9
Annabelle Wang <64243610+awctw@users.noreply.github.com>
2022-01-07 10:04:26 -0800

    first commit

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
+
+function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  function addIngredientHandler(ingredient) {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      {
+        id: Math.random().toString,
+        ...ingredient,
+      },
+    ]);
+  }
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
